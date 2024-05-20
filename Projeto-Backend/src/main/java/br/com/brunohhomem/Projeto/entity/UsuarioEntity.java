03e6e3509f233c93e1f15c77f6a78d563282f8f2
Bruno H. Homem <brunohhomem@gmail.com>
2024-05-20 11:43:31 -0300

    feat: added usuario entity

---
+++ Projeto-Backend/src/main/java/br/com/brunohhomem/Projeto/entity/UsuarioEntity.java
@@ -0,0 +1,26 @@
+package br.com.brunohhomem.Projeto.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuario")
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
